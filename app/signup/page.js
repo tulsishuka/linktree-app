05f713d98036aab07f53a7d1fd1e31500e769b8c
Tulasi Shukla <tulsishuklag@gmail.com>
2025-08-09 17:03:02 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -51,7 +51,7 @@ const [email, setemail] = useState(" ")
       redirect: "follow"
     };
 
-   const r = await fetch("http://localhost:3000/api/add", requestOptions)
+   const r = await fetch("https://linktree-app-uv1m.vercel.app/api/add", requestOptions)
    const result = await r.json()
    if(result.success){ 
      toast.success(result.message)
